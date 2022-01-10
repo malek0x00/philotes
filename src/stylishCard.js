0c09810c6bd007ef7c9700496e88de732f26af68
Malek Bougadouha <bougadouha.malek@gmail.com>
2022-01-10 22:05:01 +0100

    made cards content load from api

--- src/stylishCard.js
+++ src/stylishCard.js
@@ -21,9 +21,9 @@ function StylishCard(props) {
 
     return (
     <div style={props.style}>
-        <Card onMouseEnter={hoover} onMouseLeave={cancelhover} shadow="xl" radius='lg' style={{transform: hover,transition: 'transform 150ms ease', backgroundImage: 'url("https://www.jordanrobins.com.au/wp-content/uploads/2020/10/Morning-Ocean-Motions-_-Vertical.jpg")',backgroundRepeat: 'no-repeat', backgroundSize:'500px',width:'300px', height:'500px'}}>
-            <h1 style={{ position: 'absolute',  top:350, color:"#FFFFFF"}}>eds eds eds</h1>
-            <Text ref={ref01} style={{ position: 'absolute',  top:420, color:"#e1e1e1"}}>{props.title}</Text>
+        <Card onMouseEnter={hoover} onMouseLeave={cancelhover} shadow="xl" radius='lg' style={{transform: hover,transition: 'transform 150ms ease', backgroundImage: `url(${props.img})`,backgroundRepeat: 'no-repeat', backgroundSize:'500px',width:'300px', height:'500px'}}>
+            <h1 style={{ position: 'absolute',  top:350, color:"#FFFFFF"}}>{props.name}</h1>
+            <Text ref={ref01} style={{ position: 'absolute',  top:420, color:"#e1e1e1"}}>{props.surname}</Text>
         </Card>
     </div>
     )
--- src/userPage.js
+++ src/userPage.js
@@ -1,7 +1,7 @@
 import './App.css';
 import * as React from 'react';
-import { useState} from 'react';
-import {Grid,Col,Button, Modal} from '@mantine/core';
+import { useState, useEffect} from 'react';
+import {Grid,Col,Button, Modal,Center,Loader} from '@mantine/core';
 import "@fontsource/rouge-script";
 import background from "./images/userpagebck.png";
 import cards from "./images/cards.png"
@@ -9,37 +9,20 @@ import TinderCard from 'react-tinder-card';
 import StylishCard from './stylishCard';
 import added from './images/added.png';
 import settings from './images/settings.png';
+import axios from "axios";
 function UserPage (props) {
-
-    const characters = [
-        {
-          name: 'Richard Hendricks',
-          url: './img/richard.jpg'
-        },
-        {
-          name: 'Erlich Bachman',
-          url: './img/erlich.jpg'
-        },
-        {
-          name: 'Monica Hall',
-          url: './img/monica.jpg'
-        },
-        {
-          name: 'Jared Dunn',
-          url: './img/jared.jpg'
-        },
-        {
-          name: 'Dinesh Chugtai',
-          url: './img/dinesh.jpg'
-        }
-      ]
-
 
     const [lastDirection, setLastDirection] = useState();
     const [swipedpp, setSwipedpp] = useState([]);
     const [opened, setOpened] = useState(false);
+    const [characters, setCharacter] = useState();
 
-
+      useEffect(() => {
+        axios.get("https://reqres.in/api/users?page=2").then((response) => {
+          setCharacter(response.data);
+          
+        });
+      });
 
 
     const swiped = (direction, nameToDelete) => {
@@ -53,6 +36,25 @@ function UserPage (props) {
        
       }
 
+      if (!characters)
+      return (
+        <Center style={{ widt: "100%", height: "80vh" }}>
+          <Loader color="dark" size="xl" variant="bars" />
+        </Center>
+      );
+
+      var rows = [];
+      characters.data.map((character) => {
+        rows.push(
+        <TinderCard onSwipe={(dir) => swiped(dir, character.first_name)} onCardLeftScreen={() => outOfFrame(character.first_name)}>  
+        <div style={{width:'100%', position:'absolute',display:'flex',alignItems:'center',justifyContent: "center",alignContent:"center"}} >
+        <StylishCard style={{}} name={character.first_name} surname={character.last_name} img={character.avatar}></StylishCard>
+        </div>
+        </TinderCard>
+        );
+        return 0;
+      });
+
 
     return(
         <div>
@@ -62,14 +64,9 @@ function UserPage (props) {
              
             <img src={background} alt="background pic" style={{position:'absolute',top:0,right:0,width:'100%'}}></img>
             <div>
-            {characters.map((character) =>
-            <TinderCard onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
-                <div style={{width:'100%', position:'absolute',display:'flex',alignItems:'center',justifyContent: "center",alignContent:"center"}} >
-                <StylishCard style={{}} title={character.name}></StylishCard>
-                </div>
-              
-            </TinderCard>
-            )}
+             
+              {rows}
+
             </div>
            
             <div>
@@ -83,12 +80,12 @@ function UserPage (props) {
                 </Col>
                 <Col style={{display:'flex', alignItems:'center',alignContent:'center',justifyContent:'center', height:'100%'}} span={4}>
                 
-                <Button style={{backgroundImage:'linear-gradient(to top,#fe957e,#ffbdaf)',width:120,height:120,borderRadius:80,marginTop:-100,border: '4px solid',borderColor: '#FFFFFF'}}><img style={{width:70,height:70}} src={cards} alt="x"></img></Button>
+                  <Button style={{backgroundImage:'linear-gradient(to top,#fe957e,#ffbdaf)',width:120,height:120,borderRadius:80,marginTop:-100,border: '4px solid',borderColor: '#FFFFFF'}}><img style={{width:70,height:70}} src={cards} alt="x"></img></Button>
             
                 </Col>
                 <Col style={{display:'flex', alignItems:'center',alignContent:'center',justifyContent:'center', height:'100%'}} span={4}>
                 
-                <Button style={{backgroundImage:'linear-gradient(to top,#fe957e,#ffbdaf)',width:100,height:100,borderRadius:50,marginTop:-80,border: '4px solid',borderColor: '#FFFFFF'}}><img style={{width:50,height:50}} src={settings} alt="x"></img></Button>
+                  <Button style={{backgroundImage:'linear-gradient(to top,#fe957e,#ffbdaf)',width:100,height:100,borderRadius:50,marginTop:-80,border: '4px solid',borderColor: '#FFFFFF'}}><img style={{width:50,height:50}} src={settings} alt="x"></img></Button>
             
                 </Col>
             </Grid>
